8eac6b6dabf4c67f0cc6674edbccaa4ebfbc0e22
jmazor <jmazor@users.noreply.github.com>
2021-09-25 21:46:35 -0400

    Solution 25 Psuedocode

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution25 {
+
+    // Input a string
+
+    // initalize char count number count and special char count
+
+    // loop through each char of string
+
+    // if char count < 8 return weak
+
+    // if char count + num is > 8 && num > 1 return strong
+
+    // if char count + num is > 8 && num > 1 && specialchar > 1 return very strong
+    
+    // else return unknown
+}
