e9a24d06ba374b38fb0443c00a3f06729b1cf507
jmazor <jmazor@users.noreply.github.com>
2021-09-29 19:17:52 -0400

    Solution24

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -1,15 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
 package baseline;
+
+import java.util.Scanner;
 
 public class Solution24 {
     //create a function called anagram which takes 2 strings
     // reverses string 2 and compares
     // if equal returns true
+    static public boolean isAnagram(String a, String b) {
+        b =  new StringBuilder(b).reverse().toString();
+        if (a.equals(b))
+            return true;
+        else
+            return false;
+    }
 
 
-    //input data as strings
 
-    // call anagram function
+    public static void main(String[] args) {
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:");
+        System.out.print("Enter the first string: ");
+        //input data as strings
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        System.out.print("Enter the second string: ");
+        String b = sc.nextLine();
+        // call anagram function and print result
+
+        if (isAnagram(a,b ) == true) {
+            System.out.println("\"" + a + "\" and  \"" + b + "\" are anagrams.");
+        }
+        else
+            System.out.println("\"" + a + "\" and \"" + b + "\" are not anagrams.");
+    }
 
 
-    // return result
+
 }
