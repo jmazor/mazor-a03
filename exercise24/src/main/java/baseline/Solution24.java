d427e9c52734f5732df0f68700d30c4b342f5d40
jmazor <jmazor@users.noreply.github.com>
2021-09-29 17:38:45 -0400

    New Psuedocode

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -1,12 +1,15 @@
 package baseline;
 
 public class Solution24 {
+    //create a function called anagram which takes 2 strings
+    // reverses string 2 and compares
+    // if equal returns true
+
+
     //input data as strings
 
-    // reverse second string
+    // call anagram function
 
-    // compare second to first string
 
-    // if equals strings are anagrams
-    // else not anagrams
+    // return result
 }
