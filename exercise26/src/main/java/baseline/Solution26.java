72bfe3ba51459b8b8c9409784e1c97d6b26f64ce
jmazor <jmazor@users.noreply.github.com>
2021-09-30 16:43:33 -0400

    Solution 26 Psuedocode

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,21 @@
+package baseline;
+
+public class Solution26 {
+
+    // create a class called PaymentCalculator
+    // Class has 3 private variables APR  (as precentage) Balance and Monthly payment
+
+    // Class has one public method called calculate MonthsUntilPaidOff
+    // Takes no paramters and does the formula
+
+
+
+    // init a PaymentCalculator object called card
+    // Ask for and set
+    // balance
+    // apr
+    // monthly payment
+
+    // call card.MonthsUntilPaidOff
+
+}
