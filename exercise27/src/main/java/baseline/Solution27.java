baa7a741e572c8ef02568afeaf5d593476943d80
jmazor <jmazor@users.noreply.github.com>
2021-09-30 18:04:06 -0400

    Solution 27 Pseudo

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,28 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+
+public class Solution27 {
+
+    // validate method
+    // make sure first and last name exists
+    // first name and last name size >= 2
+    // id first 2 must be chars
+    // id[3] must be hyphen
+    // zipcode must contain only numbers
+
+    // return concated string
+
+    public static void main(String[] args) {
+        //input firstname
+        // lastname
+        // zip
+        // id
+        // call validate method
+
+    }
+}
