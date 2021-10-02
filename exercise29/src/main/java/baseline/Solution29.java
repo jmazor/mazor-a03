c4bea72e928d32aa36eb9a4112de5dbb1ba24568
jmazor <jmazor@users.noreply.github.com>
2021-10-01 23:02:17 -0400

    29 Pseudo

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class Solution29 {
+
+    // rule of 72 function
+    //takes a value divides by 72 returns an int
+
+    public static void main(String[] args) {
+
+        // for loop until data is validated
+        // if value == 0 call next
+        // if value != numeric call next
+        // else break
+
+        // call 72 funciton
+        // print output
+
+    }
+}
