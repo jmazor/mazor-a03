03789ae8aa567009cd80389a0a35bac434de92c2
jmazor <jmazor@users.noreply.github.com>
2021-10-01 23:54:31 -0400

    30 Pseudo

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class Solution30 {
+
+    //printTable method
+    // nested for loops
+    // both loops 1-12
+    // inside loop multiplies and prints both counter values
+    // print new line when inside loop ends
+
+    public static void main(String[] args) {
+        // call printTable method
+    }
+
+}
