2be3f53b6478dee6f3085b8bb6bd01021f884272
jmazor <jmazor@users.noreply.github.com>
2021-10-01 22:29:06 -0400

    28 Psuedo

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,14 @@
+package baseline;
+
+public class Solution28 {
+    // adder function
+    // takes a value
+    // asks for a number
+    // returns previous value + entered number
+
+    public static void main(String[] args) {
+        // init value to 0
+        // For loop calling adderMethod
+        // print final value
+    }
+}
