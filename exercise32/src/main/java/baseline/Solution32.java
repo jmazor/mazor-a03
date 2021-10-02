d66e2dd62580965f9b5de4b6204a72822e0f34be
jmazor <jmazor@users.noreply.github.com>
2021-10-02 19:04:21 -0400

    32 Pseudo

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,33 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+
+public class Solution32 {
+
+    public static int guessNumber(int difficulty) {
+        // int difficulty sets range
+        // I have my number ask user for guess
+        // while guess != number
+        // if guess < number return too low Guess Again:
+        // if guess >  number return too high Guess Again:
+        // ++count
+
+        // return count when while loop ends
+
+
+    }
+
+    public static void main(String[] args) {
+        // prompt user for input
+        // if 1 set range to 10
+        // 2 set range to 100
+        // 3 set range to 1000
+        // call guessNumber
+        // Print you got it in guessNumber(difficulty) tries
+
+    }
+}
