ce61af0aaafe24641328948a8e499a041615afe3
jmazor <jmazor@users.noreply.github.com>
2021-10-01 22:46:08 -0400

    Solution 28

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -1,14 +1,27 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
     // adder function
     // takes a value
-    // asks for a number
+    // takes a sum
     // returns previous value + entered number
+
+    public static int getSum(int value, int sum) {
+        return value + sum;
+    }
 
     public static void main(String[] args) {
         // init value to 0
         // For loop calling adderMethod
         // print final value
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i != 5; ++i) {
+            System.out.print("Enter a number: ");
+            sum = getSum(sc.nextInt(), sum);
+        }
+        System.out.println("The total is " + sum + ".");
     }
 }
--- exercise28/src/test/java/baseline/Solution28Test.java
+++ exercise28/src/test/java/baseline/Solution28Test.java
@@ -7,7 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class Solution28Test {
 
     @Test
-    void adderTest() {
+    void getSumSimple() {
+        assertEquals(15, Solution28.getSum(5, 10));
+    }
 
+    @Test
+    void getSumLoop () {
+        int ret = 0;
+        for (int i = 0; i != 5; ++i) {
+            ret = Solution28.getSum(i, ret);
+        }
+        assertEquals(10, ret);
     }
 }
