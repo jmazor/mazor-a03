ce4501d0afd9bb097a874e44d0bf9e0073568d09
jmazor <jmazor@users.noreply.github.com>
2021-10-01 23:59:24 -0400

    Finished 30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,5 +1,10 @@
 package baseline;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
 public class Solution30 {
 
     //printTable method
@@ -7,9 +12,17 @@ public class Solution30 {
     // both loops 1-12
     // inside loop multiplies and prints both counter values
     // print new line when inside loop ends
-
-    public static void main(String[] args) {
-        // call printTable method
+    public static void printTable() {
+        for (int i = 1; i != 13; ++i) {
+            for (int y = 1; y!= 13; ++y) {
+                System.out.printf(" %3d ", i*y);
+            }
+            System.out.println();
+        }
     }
 
+    public static void main(String[] args) {
+        //call printTable
+        printTable();
+    }
 }
