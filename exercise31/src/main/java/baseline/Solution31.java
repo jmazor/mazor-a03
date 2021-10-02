c004db3aa9cf6966f342194f62f6b7235ad57f84
jmazor <jmazor@users.noreply.github.com>
2021-10-02 04:07:15 -0400

    31 Pseudo

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,28 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+public class Solution31 {
+
+    //getHeartRate
+    // Take age and resting
+    // TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+
+    // validateInput
+    // try catch to convert String to int
+    public static void main(String[] args) {
+
+        // input age and resting
+
+
+        // print
+        // Intensity   | Rate
+        //-------------|--------
+        // for loop 55 to 95 i+= 5
+        // print i% | getHeartRate(i) in format
+
+    }
+}
