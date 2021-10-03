d96005e97e8e32e5144530f576ecb6ef8d6b5f62
jmazor <jmazor@users.noreply.github.com>
2021-10-03 17:03:56 -0400

    40 pseudo

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,24 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+import java.util.*;
+
+public class Solution40 {
+
+    public static void printSearch(List<Map<String, String>> employeeData, String searchValue) {
+        // for every item in map
+
+        //if map key contains searchValue print it
+    }
+
+    public static void main(String[] args) {
+        // input data
+        // ask for search value
+        // call printSearch
+
+    }
+}
