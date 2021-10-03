2a2740b9ab0d12ffda090dbc284f732188924f81
jmazor <jmazor@users.noreply.github.com>
2021-10-02 23:21:32 -0400

    34 pseudo

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,33 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+import java.util.List;
+
+public class Solution34 {
+
+
+    public static String[] removeRecord(String[] employeeList, String employee) {
+        // loop through array
+        // test each value at index is equal to employee
+
+        // if equals delete remove record and return new array
+    }
+
+    public static void List printList() {
+        // Print size of array
+        // loop through and print each record
+    }
+
+    public static void main(String[] args) {
+        // Init array with 5 employees
+        // Input employee to be removed
+        // printList
+        // call removeRecord(employee)
+        // printList 
+
+    }
+}
