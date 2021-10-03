2db7107192239073729c108e9cde93ddc264ad09
jmazor <jmazor@users.noreply.github.com>
2021-10-03 00:16:45 -0400

    35 Pseudo

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,32 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution35 {
+
+    public static List<String> inputData() {
+        // create an ArrayList
+        // add data until reach empty entry
+        // return list
+    }
+
+    public static String getWinner() {
+        // generate random number of list size
+        // return string at that index
+    }
+
+
+
+    public static void main(String[] args) {
+        // call input data
+        // call getWinner
+        // print winner
+
+    }
+}
