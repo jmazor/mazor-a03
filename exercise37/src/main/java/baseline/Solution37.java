bffd1ebeb33a514215e3b60de754c80446ef5753
jmazor <jmazor@users.noreply.github.com>
2021-10-03 13:31:23 -0400

    More Random Password

--- exercise37/src/main/java/baseline/Solution37.java
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -9,11 +9,6 @@ import java.util.*;
 
 public class Solution37 {
 
-    // String constants because of sonarLint
-    private static final String NUMBERS = "numNumbers";
-    private static final String LETTERS = "numLetters";
-    private static final String SPECIAL = "numSpec";
-
     public static int getNumLetters(int numNumbers, int numSpec, int minSize, int extra) {
         // set letters + numNumbers + numSpec
         int numLetters = numNumbers + numSpec;
@@ -38,74 +33,51 @@ public class Solution37 {
         }
     }
 
+
+    public static String getLetters(int numLetters, Random rand) {
+        final ArrayList<String> letterList = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
+        String ret = "";
+        for (int i = 0; i != numLetters; ++i) {
+            String letter = letterList.get(rand.nextInt(letterList.size()));
+            if (rand.nextInt(2) == 0)
+                letter = letter.toUpperCase();
+            ret = ret.concat(letter);
+        }
+        return ret;
+    }
+
+    public static String getNumbers(int numLetters, Random rand) {
+        final ArrayList<String> numList = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
+        String ret = "";
+        for (int i = 0; i != numLetters; ++i) {
+                ret = ret.concat(numList.get(rand.nextInt(numList.size())));
+        }
+        return ret;
+    }
+
+    public static String getSpecial(int numLetters, Random rand) {
+        final ArrayList<String> specialCharList = new ArrayList<>(Arrays.asList("!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "/", ":",
+                ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{" , "}", "|", "~" ));
+        String ret = "";
+        for (int i = 0; i != numLetters; ++i) {
+                ret = ret.concat(specialCharList.get(rand.nextInt(specialCharList.size())));
+        }
+        return ret;
+    }
+
+
     public static String getPassword(int numNumbers, int numSpec, int numLetters, Random rand) {
         // init string ret
         String password = "";
-        final ArrayList<String> specialCharList = new ArrayList<>(Arrays.asList("!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "/", ":",
-                ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{" , "}", "|", "~" ));
-        final ArrayList<String> numList = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
-        final ArrayList<String> letterList = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i",
-                "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
-        // create an arrayList of numNumbers, numSpec, numLetters
-        ArrayList<String> printThis = new ArrayList<>(Arrays.asList(NUMBERS, SPECIAL, LETTERS));
         // while numNumbers + specCount + letterCount != 0
-        while (numNumbers + numSpec + numLetters != 0) {
-            // String test = random nextInt.get(list.size())
-            String test = printThis.get(rand.nextInt(printThis.size()));
-            // if test = numNumbers
-            switch (test) {
-                case NUMBERS -> {
-                    if (numNumbers <= 0) {
-                        // removeEntry(numNumbers)
-                        removeEntry(printThis, NUMBERS);
-                        // SONARLINT WONT LET ME JUST USE AN ELSE
-                        break;
-                    }
 
-                    // call String ret = concat.getNum(rand)
-                    password = password.concat(numList.get(rand.nextInt(numList.size())));
+        password = password.concat(getLetters(numLetters, rand));
+        password = password.concat(getNumbers(numNumbers, rand));
+        password = password.concat(getSpecial(numSpec, rand));
 
-                    // decrease numCountCharacter.isLetter(password.charAt(i))
-                    numNumbers--;
-                }
-                // if numCount equals 0
-                case LETTERS -> {
-                    // if test = numLetters
-                    // if numLetter equals 0
-                    //remove entry numLetters
-                    if (numLetters <= 0) {
-                        removeEntry(printThis, LETTERS);
-                        // SONARLINT WONT LET ME JUST USE AN ELSE
-                        break;
-                    }
-                    // call String ret = concat.getLetter(rand)
-                    String letter = letterList.get(rand.nextInt(letterList.size()));
-                    // randomly decide if uppercase
-                    if (rand.nextInt(2) == 1) {
-                        letter = letter.toUpperCase();
-                    }
-                    password = password.concat(letter);
-                    // decrease numLetter
-                    numLetters--;
-                }
-                // if test = numSpec
-                case SPECIAL -> {
-                    // removeEntry(numSpec)
-                    if (numSpec <= 0) {
-                        removeEntry(printThis, SPECIAL);
-                        // SONARLINT WONT LET ME JUST USE AN ELSE
-                        break;
-                    }
-                    password = password.concat(specialCharList.get(rand.nextInt(specialCharList.size())));
-                    // call String ret = concat.getSpec(rand)
-                    // decrease numspec
-                    numSpec--;
-                }
-                // if numspec equals 0
-
-                default -> System.out.println("Error");
-            }
-        }
+        //Randomize order
+        password = scramble(password, rand);
         return password;
     }
 
@@ -127,6 +99,21 @@ public class Solution37 {
         }
         // returns 0 if invalid or input if valid
         return ret;
+    }
+
+    
+    public static String scramble(String password, Random rand)
+    {
+        // Convert your string into a simple char array:
+        char[] a = password.toCharArray();
+        for( int i=0 ; i != a.length ; i++ )
+        {
+            int j = rand.nextInt(a.length);
+            // Swap letters
+            char temp = a[i]; a[i] = a[j];  a[j] = temp;
+        }
+
+        return new String(a);
     }
 
 
---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,8 @@
+package baseline;
+
+public class Solution38 {
+
+    public static void main(String[] args) {
+
+    }
+}
