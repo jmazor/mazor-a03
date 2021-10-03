b48584848a85785fe81d5979771b5cc4e00f4bd2
jmazor <jmazor@users.noreply.github.com>
2021-10-03 00:38:34 -0400

    Finished 35

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -5,8 +5,10 @@ package baseline;
  *  Copyright 2021 Justin Mazor
  */
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.List;
 
 public class Solution35 {
 
@@ -14,18 +16,29 @@ public class Solution35 {
         // create an ArrayList
         // add data until reach empty entry
         // return list
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> entryList = new ArrayList<>();
+        String input = "";
+        boolean flag = true;
+        while (flag) {
+            System.out.print("Enter a name: ");
+            input = sc.nextLine();
+            if (input.isBlank())
+                flag = false;
+            else {
+                entryList.add(input);
+            }
+        }
+        return entryList;
     }
-
-    public static String getWinner() {
-        // generate random number of list size
-        // return string at that index
-    }
-
 
 
     public static void main(String[] args) {
         // call input data
-        // call getWinner
+        ArrayList<String> entryList = (ArrayList<String>) inputData();
+        // I removed the getWinner function because sonarLint complained
+        Random rand = new Random();
+        System.out.println("The winner is... " + entryList.get(rand.nextInt(entryList.size())) + ".");
         // print winner
 
     }
