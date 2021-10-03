d1976ff53b71377a71fe8f12c87e48718511b029
jmazor <jmazor@users.noreply.github.com>
2021-10-03 15:38:53 -0400

    39 Pseudo

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,34 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+import java.util.List;
+import java.util.Map;
+
+public class Solution39 {
+
+
+    public static void sortList(List<Map<String, List<String>>> list) {
+        // create a new list
+        // go through every key in the list
+        // compare string and set value to the alphabetic first string
+        // remove value from list, place on new list
+        // again until list is empty
+
+        // return new list
+    }
+
+    public static void main(String[] args) {
+        // init data 
+        // create a list contain maps
+        // the map key is a String last name, First Name and the values is a list of strings
+        // values are first name position and seperation date
+
+        // call sortList
+
+        // call printList
+    }
+}
