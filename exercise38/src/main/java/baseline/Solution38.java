8e4233b7fc685c9589840ad62b0614f0c325590c
jmazor <jmazor@users.noreply.github.com>
2021-10-03 13:51:27 -0400

    38 Pseudo

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -1,8 +1,43 @@
 package baseline;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
 public class Solution38 {
 
+    public static int[] getInput() {
+        //get next line
+
+        // for every item in line
+        // if " " ignore
+        // if !num return error and exit
+        // if num store in array
+
+        //return array
+    }
+
+    public static int[] filterEvenNumber() {
+        // for every item in error
+
+        // init new array
+
+        // test if num % 2 == 0
+        // if true store
+        // if not next
+
+
+        //return array
+    }
+
     public static void main(String[] args) {
+
+        //call getInput
+
+        //call filterEvenNumber
+
+        // print array
 
     }
 }
