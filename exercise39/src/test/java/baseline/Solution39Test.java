bf7aa2741a2a7656688f552cc98452f3d7947844
jmazor <jmazor@users.noreply.github.com>
2021-10-03 18:12:30 -0400

    39 Test

---
+++ exercise39/src/test/java/baseline/Solution39Test.java
@@ -0,0 +1,54 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution39Test {
+
+    @Test
+    void testOrder() {
+        List<Map<String, String>> employeeData = new ArrayList<>();
+        Map<String, String> employeePosition = new TreeMap<>();
+        Map<String, String> employeeSeparation = new TreeMap<>();
+        employeePosition.put("Johnson, John", "Manager");
+        employeeSeparation.put("Johnson, John", "2016-12-31");
+
+        employeePosition.put("Xiong, Tou", "Software Engineer");
+        employeeSeparation.put("Xiong, Tou", "2016-10-05");
+
+        employeePosition.put("Michaelson, Michaela", "District Manager");
+        employeeSeparation.put("Michaelson, Michaela", "2015-12-19");
+
+        employeePosition.put("Jacobson, Jake", "Programmer");
+        employeeSeparation.put("Jacobson, Jake", "");
+
+        employeePosition.put("Jackson, Jacquelyn", "DBA");
+        employeeSeparation.put("Jackson, Jacquelyn", "");
+
+        employeePosition.put("Webber, Sally", "Web Developer");
+        employeeSeparation.put("Webber, Sally", "2015-12-18");
+        employeeData.add(employeePosition);
+        employeeData.add(employeeSeparation);
+        List<String> actualOrder = new ArrayList<>();
+        for (Map.Entry<String, String> entry : employeeData.get(0).entrySet()) {
+            actualOrder.add(entry.getKey());
+        }
+
+        List<String> expectedOrder = new ArrayList<>();
+        expectedOrder.add("Jackson, Jacquelyn");
+        expectedOrder.add("Jacobson, Jake");
+        expectedOrder.add("Johnson, John");
+        expectedOrder.add("Michaelson, Michaela");
+        expectedOrder.add("Webber, Sally");
+        expectedOrder.add("Xiong, Tou");
+
+        assertEquals(expectedOrder, actualOrder);
+
+    }
+}
