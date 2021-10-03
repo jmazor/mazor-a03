57ea62503cd4db89e98e2ad1e77ec863af70ad1d
jmazor <jmazor@users.noreply.github.com>
2021-10-02 22:50:22 -0400

    Finished 33

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -1,18 +1,28 @@
 package baseline;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.List;
+
 public class Solution33 {
-
-    // return random Number
-    public static int getRand() {
-
-    }
 
 
     public static void main(String[] args) {
         //Ask What is your question
-
+        System.out.print("What's your question?\n> ");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        Random rand =  new Random();
         // init array answers[] with "Yes" "No" "Maybe" "Ask again later." (in this order)
+        List<String> answers = new ArrayList<>(List.of("Yes", "No", "Maybe", "Ask again later."));
         // call getRand()
         // print  answers[getRand()]
+        System.out.println(answers.get(rand.nextInt(4)));
     }
 }
