46b1ca2b8c18ce611969e8965040af703d2b29df
jmazor <jmazor@users.noreply.github.com>
2021-10-02 22:33:08 -0400

    33 Pseudo

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution33 {
+
+    // return random Number
+    public static int getRand() {
+
+    }
+
+
+    public static void main(String[] args) {
+        //Ask What is your question
+
+        // init array answers[] with "Yes" "No" "Maybe" "Ask again later." (in this order)
+        // call getRand()
+        // print  answers[getRand()]
+    }
+}
